78e1865359d11731ef43bf8b1055c88deea3f080
Stephan <st3phanvdelst@gmail.com>
2019-02-06 10:35:05 +0100

    Added client class

--- product.php
+++ product.php
@@ -10,5 +10,20 @@ class Product{
         $this->price = $price;
     }
 }
+
+class Client {
+    public $name;
+    public $age;
+    public $clientNumber;
+
+    public function __construct($name, $age, $clientNumber)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->clientNumber = $clientNumber;
+    }
+}
+
+
 $product = new Product('Big Mac', '4.99');
 var_dump($product);
